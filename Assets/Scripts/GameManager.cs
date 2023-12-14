ddba273844c128c6ff998f253bf7c1c1a0c69861
Borges <matheusborges2051@gmail.com>
2023-12-14 15:46:22 -0300

    atualizações

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,13 +2,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Ebac.Core.Singleton;
+using DG.Tweening;
 
 public class GameManager : Singleton<GameManager>
 {
     [Header("Player")]
-    public GameObject player;
+    public GameObject playerPrefab;
 
     [Header("Enemies")]
     public List<GameObject> enemies;
 
+    [Header("References")]
+    public Transform startPoint;
+
+    [Header("Animation")]
+    public float duration = .2f;
+    public float delay = .05f;
+    public Ease ease = Ease.OutBack;
+
+    private GameObject _currentPlayer;
+
+    private void Start()
+    {
+        Init();
+    }
+
+    public void Init()
+    {
+        SpawnPlayer();
+    }
+
+    private void SpawnPlayer()
+    {
+        _currentPlayer = Instantiate(playerPrefab);
+        _currentPlayer.transform.position = startPoint.transform.position;
+        _currentPlayer.transform.DOScale(0, duration).SetEase(ease).From();
+    }
 }
