7aef6526c8aaee401bd700caad9ce1507b7a9978
Borges <matheusborges2051@gmail.com>
2024-01-18 17:34:54 -0300

    adding run key

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -7,7 +7,9 @@ public class Player : MonoBehaviour
     public Rigidbody2D myRigidbody;
     public Vector2 friction = new Vector2(-.1f,0);
     public float speed; 
+    public float speedRun;
     public float forceJump = 2;
+    private float _currentSpeed;
     private void Update()
     {
         HandleJump();
@@ -16,16 +18,25 @@ public class Player : MonoBehaviour
 
     private void HandleMoviments()
     {
-        
+            if(Input.GetKey(KeyCode.LeftShift))
+            {
+                _currentSpeed = speedRun;
+            }
+
+            else
+            {
+                _currentSpeed = speed;
+            }
+
             if(Input.GetKey(KeyCode.LeftArrow))
             {
                 //myRigidbody.MovePosition(myRigidbody.position - velocity * Time.deltaTime);
-                myRigidbody.velocity = new Vector2(- speed, myRigidbody.velocity.y);
+                myRigidbody.velocity = new Vector2(- _currentSpeed, myRigidbody.velocity.y);
             }
             else if(Input.GetKey(KeyCode.RightArrow))
             {
                 //myRigidbody.MovePosition(myRigidbody.position + velocity * Time.deltaTime); 
-                myRigidbody.velocity = new Vector2(speed, myRigidbody.velocity.y);
+                myRigidbody.velocity = new Vector2(_currentSpeed, myRigidbody.velocity.y);
             }
 
             if(myRigidbody.velocity.x > 0 )
