646a5878c1a685a3a55a677f0be8995bf861a3b9
Borges <matheusborges2051@gmail.com>
2024-02-02 14:39:54 -0300

    Update itemManager/Add new sprite

--- Assets/Scripts/Coin/ItemManager.cs
+++ Assets/Scripts/Coin/ItemManager.cs
@@ -1,24 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Ebac.Core.Singleton;
 
-public class ItemManager : MonoBehaviour
+public class ItemManager : Singleton<ItemManager>
 {
-    public static ItemManager Instance;
-    public int coins;
     
-
-    private void Awake()
-    {
-
-        if(Instance == null)
-            Instance = this;
-
-        else
-            Destroy(gameObject);
-        
-
-    }
+    public int coins;
 
     private void start()
     {
